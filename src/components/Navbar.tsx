ba333445892cb6ec0d3ea6983a923ddd83c20c4a
Germán Pablo Gonzalez <germangonzalezdev@gmail.com>
2025-05-11 11:58:28 -0300

    Agregando estilos al navlink

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,12 @@
+import { NavLink } from "react-router-dom"
 import { GrCart } from "react-icons/gr";
 import { IoIosLogIn } from "react-icons/io";
 import Logo from "../assets/logo.png";
 
 const Navbar = () => {
+
+    const ActiveStyle = 'border-b-2 border-primario pb-1 ';
+
   return (
     <nav className="text-primario mt-1 flex justify-between items-center border-b-1 shadow-md p-5 fixed top-0 left-0 right-0 bg-white z-10">
         <div>
@@ -10,9 +14,38 @@ const Navbar = () => {
         </div>
         <div>
             <ul className="flex gap-4 text-black">
-                <li>Celulares</li>
-                <li>Nosotros</li>
-                <li>Contacto</li>
+                <li>
+                    <NavLink 
+                        to="/"
+                        className={({ isActive }) => isActive ? ActiveStyle : undefined}
+                    >
+                        Home
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/celulares"
+                        className={({ isActive }) => isActive ? ActiveStyle : undefined}
+                    >
+                        Celulares
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/nosotros"
+                        className={({ isActive }) => isActive ? ActiveStyle : undefined}
+                    >
+                        Nosotros
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to="/contacto"
+                        className={({ isActive }) => isActive ? ActiveStyle : undefined}
+                    >
+                        Contacto
+                    </NavLink>
+                </li>
             </ul>
         </div>
         <div>
