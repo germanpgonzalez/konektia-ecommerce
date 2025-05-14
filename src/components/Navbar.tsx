bdd8dfdee357857df3238626ea2332d0985de1ff
Germán Pablo Gonzalez <germangonzalezdev@gmail.com>
2025-05-14 09:38:40 -0300

    Estilos navbar corregidos

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <header>
-      <nav className="text-primario mt-1 flex justify-between items-center border-b-1 shadow-md p-5 fixed top-0 left-0 right-0 bg-white z-10">
+      <nav className="text-primario mt-0 flex justify-between items-center border-b-1 shadow-md p-3 fixed top-0 left-0 right-0 bg-white z-10">
         <div>
           <img src={Logo} alt="logo" width={200} />
         </div>
