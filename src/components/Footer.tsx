092b3dabbd4ad1c47b17eecbe8b1d66490309aa9
Germán Pablo Gonzalez <germangonzalezdev@gmail.com>
2025-05-16 19:40:54 -0300

    Actualizando el Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import { AiFillTikTok } from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <footer className="bg-primario text-white py-4 mt-30">
+    <footer className="bg-gradient-to-tr from-[#a0008b] to-[#320030] text-white py-10 px-6 shadow-inner mt-30">
         <div>
             <ul className="flex justify-center gap-4 mt-4">
                 <li><BsWhatsapp /></li>
@@ -15,7 +15,7 @@ const Footer = () => {
             </ul>
         </div>
         <div className="text-center mt-4">
-            <p>© 2025 Todos los derechos reservados</p>
+            <p>© 2025 Konektia Smartphones. Todos los derechos reservados.</p>
         </div>
     </footer>
   )
