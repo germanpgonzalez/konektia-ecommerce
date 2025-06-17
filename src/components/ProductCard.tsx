a9189d0609843a18a2360f13e79692ffd4ba1c0a
Germán Pablo Gonzalez <germangonzalezdev@gmail.com>
2025-06-17 17:26:00 -0300

    Arreglando botón detalles

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,16 +1,15 @@
 import { CartContext } from "../Context";
-import { useContext } from "react"
+import { useContext } from "react";
 import type { ProductType } from "./ProductList";
-import { Link } from "react-router-dom";
+
 
 type ProductCardProps = {
   product: ProductType;
   onViewDetails: (product: ProductType) => void;
 };
 
-export const ProductCard = ({product, onViewDetails }: ProductCardProps) => {
-
-  const context = useContext(CartContext)
+export const ProductCard = ({ product, onViewDetails }: ProductCardProps) => {
+  const context = useContext(CartContext);
 
   if (!context) {
     return <p>Error: contexto del carrito no disponible</p>;
@@ -18,20 +17,31 @@ export const ProductCard = ({product, onViewDetails }: ProductCardProps) => {
 
   const addProductsToCart = (product: ProductType) => {
     context.setCartProducts([...context.cartProducts, product]);
-    console.log('CARD: ', context.cartProducts)
-  }
+    console.log("CARD: ", context.cartProducts);
+  };
 
   return (
     <div className="bg-white shadow-md rounded-lg p-3 text-center flex flex-col justify-center items-center max-w-sm mt-3">
-      <img src={product.thumbnail} alt={product.title}/>
+      <img src={product.thumbnail} alt={product.title} />
       <h2 className="text-lg font-semibold mt-2">{product.title}</h2>
       <p className="text-gray-700 text-base">${product.price}</p>
-      <p className="text-gray-500 text-xs mt-1 font-extralight">{product.brand}</p>
+      <p className="text-gray-500 text-xs mt-1 font-extralight">
+        {product.brand}
+      </p>
       <div className="flex gap-2 pt-4 justify-center items-center">
-        
-          <button className="text-primario border border-primario hover:bg-primario hover:text-white rounded-2xl py-2 px-4 cursor-pointer text-sm min-w-[100px] text-center transition-colors duration-200" onClick={() => onViewDetails(product)}>Detalles</button>
-        
-        <button className="text-white bg-primario hover:bg-primario/80 rounded-2xl py-2 px-4 cursor-pointer text-sm min-w-[100px] text-center transition-colors duration-200" onClick={() => addProductsToCart(product)}>Agregar</button>
+        <button
+          className="text-primario border border-primario hover:bg-primario hover:text-white rounded-2xl py-2 px-4 cursor-pointer text-sm min-w-[100px] text-center transition-colors duration-200"
+          onClick={() => onViewDetails(product)}
+        >
+          Detalles
+        </button>
+
+        <button
+          className="text-white bg-primario hover:bg-primario/80 rounded-2xl py-2 px-4 cursor-pointer text-sm min-w-[100px] text-center transition-colors duration-200"
+          onClick={() => addProductsToCart(product)}
+        >
+          Agregar
+        </button>
       </div>
     </div>
   );
